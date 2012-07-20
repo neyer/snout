be045a44123dffd0e47235119ab0b64d3e42340f
mark neyer <mark@popover.com>
2012-07-19 18:05:47 -0700

    added the code and updated readme.

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-snout
-=====
+#snout
 
-python url route generation via inspection
+## django url generation via inspection
+
+## what
+use `snout` to generate routes for your django apps using inspection, so you can do more interesting things.
+
+instead of this:
+
+    # breakfast/urls.py
+    urlpatterns =  patterns('',
+        url(r'^$','breakfast.views.index'),
+        url(r'^order/$''', 'breakfast.views.order'),
+        url(r'^waffles/$''', 'breakfast.views.waffles'),
+        url(r'^sausage/$''', 'breakfast.views.sausage'),
+    )
+ 
+
+django apps can do this:
+
+    # breakfast/urls.py
+    urlpatterns = snout.make_django_patterns(breakfast)
+
+awww yeah
+
+
+## how
+
+ `ham_and_eggs` will be assigned to the route `ham-and-eggs`
+
+this way, when you want to add more items to your breakfast web app, you can just add the `ham_and_eggs` function
---
+++ snout/__init__.py
@@ -0,0 +1,70 @@
+__doc__ = """
+generate routes for methods using inspection. 
+
+given a view like this:
+
+def a_method_like_this(request,some_arg,another_arg): pass
+
+
+snout will generate a route like this:
+    ^a-method-like-this/(?P<some_arg>[\w\-\.]+)/(?P<another_arg>[\w\-\.]+)/?$
+"""
+import inspect
+import unittest
+
+
+def starts_with_underscore(s):
+    "does this name start with an underscore? this is the default test for non-view methods."
+    return s.startswith("_")
+
+def underscores_to_dashes(s):
+    "turn underscores to dashes, because dashes are just so much more dashing"
+    return s.replace("_","-")
+
+
+def make_url_pattern(for_method,name_cleaner=underscores_to_dashes):
+    args, varargs, varkw, defaults = inspect.getargspec(for_method)
+    route_name= name_cleaner(for_method.__name__)
+
+    #we ignore the first argument, because it is always 'requests'
+    argument_components = [ '(?P<%s>[\w\-\.]+)' % arg for arg in args[1:] ]
+
+    return "/".join(["^" + route_name] + argument_components + [ "?$"])  
+    
+
+def generate_url_routes(module, private_tester=starts_with_underscore,
+                                  name_cleaner=underscores_to_dashes):
+    """inspect all functions in `module` and create a url patterns for each one."""
+
+    all_methods = inspect.getmembers(module,inspect.isfunction)
+
+    views = [ (name_cleaner(name), method) for\
+                     name,method in all_methods if not private_tester(name)]  
+
+    #extract any parameters from this function and use them to build the url 
+    for name, method in views:
+        yield make_url_pattern(method,name_cleaner), method
+
+
+def make_django_patterns(module, prefix='', private_tester=starts_with_underscore,
+                        name_cleaner=underscores_to_dashes):
+
+    from django.conf.urls import patterns, url
+    routes = [ url(regex,method)  for regex,method in\
+                     generate_url_routes(module,private_tester,name_cleaner)]
+    return patterns(prefix,*routes)
+
+
+def _this_should_not_be_a_route(its_here_for_tests_ok): pass
+
+class SnoutTest(unittest.TestCase):
+    def test_all_routes(self):
+        "ROUTECEPTION"
+        snout = inspect.getmodule(SnoutTest)
+
+        all_snout_functions = inspect.getmembers(snout,inspect.isfunction)
+        should_have_routes = [ name for name,f in all_snout_functions if not\
+                                starts_with_underscore(name)]
+
+        all_routes = list(generate_url_routes(snout))
+        self.assertEqual(len(should_have_routes),len(all_routes))
